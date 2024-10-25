4cb9ff06dc2ab5558007ed49a8f49ecd76948513
Campbell-E-Soup <cechudley@pstcc.edu>
2024-10-25 01:11:09 -0400

    added get firstString to IOSystem

---
+++ Mad-Libs/Classes/IOSystem.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mad_Libs_App.Classes
+{
+	internal class IOSystem
+	{
+		public static string GetFirstString()
+		{
+			//file path for when we are still editing in vs code
+				string filePath = Path.Combine(AppContext.BaseDirectory, "..","..","..","Text Files","default.txt");
+			if (!File.Exists(filePath)) 
+			{
+				//file for published app - above path should be removed before publishing, or we could keep it just in case.
+				filePath = Path.Combine(AppContext.BaseDirectory,"Text Files", "default.txt");
+			}
+			string output = string.Empty;
+			
+			try
+			{
+				StreamReader sr = new StreamReader(filePath);
+				string? o = sr.ReadLine();
+				if (o != null) output = o;
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+				Clipboard.SetText(ex.Message);
+			}
+			return output;
+		}
+
+		public static string GetFirstString(string filePath)
+		{
+			//file path should always exist if we use OpenFileDialog to get txt files.
+			string output = string.Empty;
+			try
+			{
+				StreamReader sr = new StreamReader(filePath);
+				string? o = sr.ReadLine();
+				if (o != null) output = o;
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+			return output;
+		}
+		public static List<string> GetAllStrings() 
+		{
+			//file path for when we are still editing in vs code
+			string filePath = Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "Text Files", "default.txt");
+			if (!File.Exists(filePath))
+			{
+				//file for published app - above path should be removed before publishing, or we could keep it just in case.
+				filePath = Path.Combine(AppContext.BaseDirectory, "Text Files", "default.txt");
+			}
+			List<string> output = new List<string>();
+
+			try
+			{
+				StreamReader sr = new StreamReader(filePath);
+				string? o = sr.ReadLine();
+				while (o != null)
+				{
+					output.Add(o);
+					o = sr.ReadLine();
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+				Clipboard.SetText(ex.Message);
+			}
+			return output;
+		}
+	}
+}
--- Mad-Libs/Prompts.cs
+++ Mad-Libs/Prompts.cs
@@ -3,88 +3,89 @@ using System.Text.RegularExpressions;
 
 namespace Mad_Libs_App
 {
-    public partial class Prompts : Form
-    {
-        MadLib? madlib;
-        Word? Replacer;
-        public Prompts()
-        {
-            InitializeComponent();
-        }
+	public partial class Prompts : Form
+	{
+		MadLib? madlib;
+		Word? Replacer;
+		public Prompts()
+		{
+			InitializeComponent();
+		}
 
-        private void Prompts_Load(object sender, EventArgs e)
-        {
-            string str = "One [adj] day, I was walking my [color] pet [noun] when [pnoun] started flying from the sky! I called my friend [name] and she said one just landed right on her [place]! [adv], it was raining [pnoun] as well and they were just going everywhere! My [noun] started [ving] and catching [pnoun] with his mouth to eat!";
-            //str = "this story has nothing to replace";
-            //str = "Noun: [noun], Adjective: [adj], Verb: [verb], Ving: [ving], Adverb: [adv], Place: [place], Person: [person], Plural Noun: [pnoun], Name: [pname], Color: [color], Body Part: [body], Plural Body Part: [pbody], Food: [food], Exclamation: [exc]";
-            madlib = new MadLib(str);
-            Replacer = madlib.Next();
-            if (Replacer != null)
-            {
-                lblPrompt.Text = Replacer.GetPrompt();
-                lblReminder.Text = Word.Examples[Replacer.Type];
-            }
-            else
-            {
-                StoryForm storyForm = new StoryForm();
-                storyForm.Tag = str;
-                //cannot hide form in load event.
-                    hideTimer.Enabled = true;
-                storyForm.Closed += (s, args) => this.Close();
-                storyForm.Show();
-            }
-        }
+		private void Prompts_Load(object sender, EventArgs e)
+		{
+			string str = "One [adj] day, I was walking my [color] pet [noun] when [pnoun] started flying from the sky! I called my friend [name] and she said one just landed right on her [place]! [adv], it was raining [pnoun] as well and they were just going everywhere! My [noun] started [ving] and catching [pnoun] with his mouth to eat!";
+			//str = "this story has nothing to replace";
+			//str = "Noun: [noun], Adjective: [adj], Verb: [verb], Ving: [ving], Adverb: [adv], Place: [place], Person: [person], Plural Noun: [pnoun], Name: [pname], Color: [color], Body Part: [body], Plural Body Part: [pbody], Food: [food], Exclamation: [exc]";
+			str = IOSystem.GetFirstString();
+			madlib = new MadLib(str);
+			Replacer = madlib.Next();
+			if (Replacer != null)
+			{
+				lblPrompt.Text = Replacer.GetPrompt();
+				lblReminder.Text = Word.Examples[Replacer.Type];
+			}
+			else
+			{
+				StoryForm storyForm = new StoryForm();
+				storyForm.Tag = str;
+				//cannot hide form in load event.
+				hideTimer.Enabled = true;
+				storyForm.Closed += (s, args) => this.Close();
+				storyForm.Show();
+			}
+		}
 
-        private void btnNext_Click(object sender, EventArgs e)
-        {
-            if (answerValidation(txtWord.Text) && Replacer != null)
-            {
-                Replacer.Replace = txtWord.Text.Trim();
-                Replacer = madlib.Next();
-                //if list is NOT empty
-                if (Replacer != null)
-                {
-                    lblPrompt.Text = Replacer.GetPrompt();
-                    lblReminder.Text = Word.Examples[Replacer.Type];
-                    btnNext.Enabled = false;
-                    txtWord.Text = string.Empty;
-                }
-                else
-                {
-                    //list is empty, story is complete
-                    this.Hide();
-                    madlib.Finish();
-                    StoryForm storyForm = new StoryForm();
-                    storyForm.Tag = madlib.FinishedStory;
-                    storyForm.Closed += (s, args) => this.Close();
-                    storyForm.Show();
-                }
-            }
-        }
+		private void btnNext_Click(object sender, EventArgs e)
+		{
+			if (answerValidation(txtWord.Text) && Replacer != null)
+			{
+				Replacer.Replace = txtWord.Text.Trim();
+				Replacer = madlib.Next();
+				//if list is NOT empty
+				if (Replacer != null)
+				{
+					lblPrompt.Text = Replacer.GetPrompt();
+					lblReminder.Text = Word.Examples[Replacer.Type];
+					btnNext.Enabled = false;
+					txtWord.Text = string.Empty;
+				}
+				else
+				{
+					//list is empty, story is complete
+					this.Hide();
+					madlib.Finish();
+					StoryForm storyForm = new StoryForm();
+					storyForm.Tag = madlib.FinishedStory;
+					storyForm.Closed += (s, args) => this.Close();
+					storyForm.Show();
+				}
+			}
+		}
 
-        private bool answerValidation(string s)
-        {
-            if (!Regex.IsMatch(s, @"^[a-zA-Z\s]+$")) { return false; }
-            if (s.Count(char.IsLetter) < 3) { return false; }
+		private bool answerValidation(string s)
+		{
+			if (!Regex.IsMatch(s, @"^[a-zA-Z\s]+$")) { return false; }
+			if (s.Count(char.IsLetter) < 3) { return false; }
 
-            return true;
-        }
+			return true;
+		}
 
-        private void txtWord_TextChanged(object sender, EventArgs e)
-        {
-            if (answerValidation(txtWord.Text))
-            {
-                btnNext.Enabled = true;
-            }
-            else
-            {
-                btnNext.Enabled = false;
-            }
-        }
+		private void txtWord_TextChanged(object sender, EventArgs e)
+		{
+			if (answerValidation(txtWord.Text))
+			{
+				btnNext.Enabled = true;
+			}
+			else
+			{
+				btnNext.Enabled = false;
+			}
+		}
 
-        private void hideTimer_Tick(object sender, EventArgs e)
-        {
-            this.Hide();
-        }
-    }
+		private void hideTimer_Tick(object sender, EventArgs e)
+		{
+			this.Hide();
+		}
+	}
 }
