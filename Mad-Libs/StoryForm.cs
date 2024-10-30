0444951e1a1bb95e821c33c7777ef6f4647601b1
Campbell-E-Soup <cechudley@pstcc.edu>
2024-10-30 09:45:32 -0400

    test

--- Mad-Libs/StoryForm.cs
+++ Mad-Libs/StoryForm.cs
@@ -37,8 +37,13 @@ namespace Mad_Libs_App
             SaveFileDialog saveFileDialog = new SaveFileDialog();
             saveFileDialog.Filter = "Text Files (*.txt)|*.txt";
 
-            saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-
+            //saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string filePath = Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "Text Files");
+            if (!Directory.Exists(filePath))
+            {
+                filePath = Path.Combine(AppContext.BaseDirectory, "Text Files");
+            }
+            saveFileDialog.InitialDirectory = filePath;
             saveFileDialog.FileName = "finished_story.txt";
 
             if (saveFileDialog.ShowDialog() == DialogResult.OK)
