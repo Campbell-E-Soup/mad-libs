50eefec84f00db7c47daec97c095eddb6f9b1800
MorganRLowe <lowemorgan98@gmail.com>
2024-11-13 09:11:04 -0500

    added a few comments for download button

--- Mad-Libs/StoryForm.cs
+++ Mad-Libs/StoryForm.cs
@@ -35,12 +35,13 @@ namespace Mad_Libs_App
             lblMessage.Text = "Story copied to clipboard!";
         }
 
+        //button for downloading finished madlib to desktop 
         private void DownloadButton_Click(object sender, EventArgs e)
-        {
+        {   //dialog showing location where user can save .txt file 
             SaveFileDialog saveFileDialog = new SaveFileDialog();
             saveFileDialog.Filter = "Text Files (*.txt)|*.txt";
 
-            //saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            //determine directory to save file 
             string filePath;
             filePath = Path.Combine(AppContext.BaseDirectory, "Text Files");
             if (!Directory.Exists(filePath))
@@ -53,15 +54,16 @@ namespace Mad_Libs_App
             }
             saveFileDialog.InitialDirectory = filePath;
             saveFileDialog.FileName = "saved_madlib.txt";
-
+            //Lets user know save was successful 
             if (saveFileDialog.ShowDialog() == DialogResult.OK)
             {
                 try
-                {
+                {   //saving to a .txt file 
                     File.WriteAllText(saveFileDialog.FileName, lblStory.Text);
                     lblMessage.ForeColor = SystemColors.ControlDark;
                     lblMessage.Text = "Story saved successfully!";
                 }
+                //Lets user know if save had issues 
                 catch (Exception ex)
                 {
                     lblMessage.ForeColor = Color.Red;
